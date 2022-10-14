d2a9f3d33ab437ee948238d0db18b243df8069e6
Sabuj Golui <sabujgolui5#gmail.com>
2022-10-14 12:39:29 +0530

    removed un neceessery files

--- src/App.js
+++ src/App.js
@@ -2,31 +2,18 @@
 import Header from './Header.js';
 import Homepage from './Homepage.js';
 import Footer from './Footer';
+import JobData from './JobData.js';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
 function App() {
-  let jobs = [
-    {
-      jobId: 242353253,
-      jobType: "Transportation",
-      jobStatus: "NOT STARTED",
-      startingDate: "10/03/2022"
-    },
-    {
-      jobId: 242535355,
-      jobType: "Storage",
-      jobStatus: "ACTIVE",
-      startingDate: "10/03/2022"
-    },
-    {
-      jobId: 476554232,
-      jobType: "Processing",
-      jobStatus: "FINISHED",
-      startingDate: "10/03/2022"
-    }
-  ]
   return (
 <homepage>
   <Header/>
-  <Homepage jobs ={jobs}/>
+  <Homepage jobs ={JobData}/>
   <Footer/>
 </homepage>
   );
--- src/Job.js
+++ src/Job.js
@@ -8,6 +8,7 @@ export const Job = ({job, index}) => {
       <td>{job.jobType}</td>
       <td>{job.jobStatus}</td>
       <td>{job.startingDate}</td>
+      <td>{job.endDate}</td>
     </tr>
   )
 }
